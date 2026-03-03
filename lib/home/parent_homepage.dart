20e3b78507bb709d2ec93cdbe8b1c0676af660ef
unknown <vuquangtung2004@gmail.com>
2026-03-04 03:07:51 +0700

    feat: configure firebase and update homepage

--- lib/home/parent_homepage.dart
+++ lib/home/parent_homepage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'
 
 class ParentHomepage extends StatelessWidget {
   const ParentHomepage({super.key});
